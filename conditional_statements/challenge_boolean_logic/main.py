f6d2f1be3195e40bca5f8ced01e226a735a03b3b
DrGS-fpv <>
2026-01-11 11:20:04 +0000

    Challenge: Boolean Logic

--- conditional_statements/challenge_boolean_logic/main.py
+++ conditional_statements/challenge_boolean_logic/main.py
@@ -3,3 +3,11 @@ on_sale = False
 selling_well = False
 current_stock = 150
 high_stock_threshold = 100
+# define boolean variable - overstock_risk
+overstock_risk = seasonal and (current_stock >= high_stock_threshold)
+# define boolean variable - discount_eligible
+discount_eligible = not selling_well and not on_sale
+# create boolean variable - make_discount
+make_discount = overstock_risk or discount_eligible
+
+print("Should the item be discounted?", make_discount)
